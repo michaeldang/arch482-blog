b6085f5dffd1cd3821bf2f06c03f704c52e45fb4
Michael Dang <michael.w.dang@gmail.com>
2016-03-08 20:46:14 -0800

    Added flash messages to the site and controller.

--- controllers/posts.js
+++ controllers/posts.js
@@ -3,31 +3,44 @@
  */
 
 var express = require('express'),
-    router = express.Router()
-var sanitizer = require('sanitizer')
-var moment = require('moment')
-var dbconfig = require('./../config/database')
-var mysql = require('mysql'),
-    connection = mysql.createConnection(dbconfig.connection)
+    router = express.Router(),
+    sanitizer = require('sanitizer'),
+    moment = require('moment'),
+    dbconfig = require('./../config/database'),
+    mysql = require('mysql'),
+    connection = mysql.createConnection(dbconfig.connection),
+    flash = require('connect-flash')
 
 connection.query('USE ' + dbconfig.database);
 
+router.use(flash());
+
 router.get('/post', isLoggedIn, function (req, res) {
     res.render('post.ejs', {
-        user: req.user
+        user: req.user,
+        flash: req.flash()
     });
 });
 
 router.post('/post', isLoggedIn, function (req, res) {
     var title = sanitizer.escape(req.body.postTitle);
     var comment = sanitizer.escape(req.body.postComment);
-    var date = moment().format('YYYY-MM-DD HH:MM:SS');
-    var posterId = req.user.id;
-    var insertQuery = "INSERT INTO posts (posterId, title, comment, date) values (?,?,?,?)";
-    connection.query(insertQuery, [posterId, title, comment, date], function (err, rows) {
-        res.redirect('/');
-    });
+    if (!title || title === "" || !title.trim()) {
+        req.flash('errorMessage', 'Title of post cannot be blank');
+        res.redirect('back');
+    } else if (!comment || comment === "" || !comment.trim()) {
+        req.flash('errorMessage', 'Comment field cannot be blank');
+        res.redirect('back');
+    } else {
+        var date = moment().format('YYYY-MM-DD HH:MM:SS');
+        var posterId = req.user.id;
+        var insertQuery = "INSERT INTO posts (posterId, title, comment, date) values (?,?,?,?)";
+        connection.query(insertQuery, [posterId, title, comment, date], function (err, rows) {
+            res.redirect('/?postSuccessful=true');
+        });
+    }
 });
+
 
 
 // route middleware to make sure
